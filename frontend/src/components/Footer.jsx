d9a344d90ff288a90260105cd302db81d837adb7
Cocci38 <sandra.villot038@gmail.com>
2023-07-05 12:12:22 +0200

    header style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,10 +3,10 @@
 export const Footer = () => {
     return (
         <footer>
-            <div className="flexRow" style={{width: '600px'}}>
+            <div className="flexRow">
                 <img src="/src/assets/Logo_ter.png" alt='logo magic book' />
-                <p>Magic-books est une application dédié aux livres et aux lecteurs.
-                    Il permet de créer et d’organiser sa bibliothèque en ligne, d’obtenir des informations sur des œuvres.
+                <p>Magic-books est une application dédié aux livres et aux lecteurs.<br></br>
+                Il permet de créer et d’organiser sa bibliothèque en ligne, d’obtenir des informations sur des œuvres.
                 </p>
             </div>
             <p className="notice"><span className="colorOrange">Magic Books</span> - Tous droits réservés - <span className="colorOrange">Mention Légale</span></p>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,12 @@ export const Header = () => {
     return (
         <div className='headerMenu'>
             <img src="/src/assets/mikolaj-DCzpr09cTXY-unsplash.jpg" className='imageHeader' />
+            <div className='titleBlock'>
+                <p> Avec Magic Books, découvrez les dernières sorties littéraires,
+                    créez votre bibliothèque virtuelle et suivez votre historique de lecture
+                </p>
+                <p>Pas encore de compte ? <span className="colorOrange">S'inscrire</span></p>
+            </div>
         </div>
     )
 }
