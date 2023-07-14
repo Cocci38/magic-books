9c4e6f088655f96516519560005bab4ad3f191ce
Cocci38 <sandra.villot038@gmail.com>
2023-07-14 10:45:57 +0200

    style page 404

--- frontend/src/utils/Error.jsx
+++ frontend/src/utils/Error.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
-            Error 404 !
-        </div>
+        <main className='error-page'>
+            <div className='error-content'>
+                <h2>404</h2>
+                <h3>Oups ! Page non trouvé</h3>
+                <p>Désolé, la page que vous recherchez n'existe pas.</p>
+                <Link to={"/"} className='button' >Revenir à l'accueil</Link>
+            </div>
+        </main>
     );
 };
 
